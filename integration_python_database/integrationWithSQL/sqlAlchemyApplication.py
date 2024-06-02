2c6c29bb7799a13c60a315ab5e243024e4facfa8
jones-bass <jonesbass.tb@gmail.com>
2024-06-02 09:32:38 -0300

    Criando tabela banco de dados

---
+++ integration_python_database/integrationWithSQL/sqlAlchemyApplication.py
@@ -0,0 +1,54 @@
+"""
+    Primeiro programa de integração com banco de dados
+    utilizando SQLAlchemy e modelo ORM
+
+"""
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from sqlalchemy import select
+from sqlalchemy import func
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+        Esta classe representa a tabela user_account dentro
+        do SQlite.
+    """
+    __tablename__ = "user_account"
+    # atributos
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+
+    address = relationship(
+        "Address", back_populates="user", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"User(id={self.id}, name={self.name}, fullname={self.fullname})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+    id = Column(Integer, primary_key=True)
+    email_address = Column(String(30), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+
+    user = relationship("User", back_populates="address")
+
+    def __repr__(self):
+        return f"Address(id={self.id}, email_address={self.email_address})"
+
+
+print(User.__tablename__)
+print(Address.__tablename__)
+
