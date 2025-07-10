a65c9f2988f227f43467e13b3e951bb9da436c8c
jones-bass <jonesbass.tb@gmail.com>
2025-07-10 19:58:29 -0300

    Add Rota delete e adptando Role no projeto

--- PythonRocketseat/Modulo_4/app.py
+++ PythonRocketseat/Modulo_4/app.py
@@ -1,89 +1,127 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from flask_login import (
+    LoginManager,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
-#view login
-login_manager.login_view = 'login'
-#Session <- conexão ativa
+# view login
+login_manager.login_view = "login"
+# Session <- conexão ativa
+
 
 @login_manager.user_loader
 def load_user(user_id):
-  return User.query.get(user_id)
+    return User.query.get(user_id)
 
-@app.route('/login', methods=["POST"])
+
+# Rota Logar --------------
+@app.route("/login", methods=["POST"])
 def login():
-  data = request.json
-  username = data.get("username")
-  password = data.get("password")
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
 
-  if username and password:
-   # Login
-    user = User.query.filter_by(username=username).first()
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
-      login_user(user)
-      print(current_user.is_authenticated)
-      return jsonify({"message": "Autenticação realizada com sucesso!"})
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
+            return jsonify({"message": "Autenticação realizada com sucesso!"})
 
-  return jsonify({"message": "Credenciais inválidas"}), 400
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
-@app.route('/logout', methods=['GET'])
+
+# Rota Logout --------------
+@app.route("/logout", methods=["GET"])
 @login_required
 def logout():
-  logout_user()
-  return jsonify({"message": "Logout realizado com sucesso!"})
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
-@app.route('/user', methods=["POST"])
+
+# Rota Criar User --------------
+@app.route("/user", methods=["POST"])
 def create_user():
-  data = request.json
-  username = data.get("username")
-  password = data.get("password")
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    role = data.get("role", "user")
 
-  if username and password:
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify({"message": "Usuario cadastrado com sucesso"})
+    if role not in ["user", "admin"]:
+        return jsonify({"message": "Role inválida. Use 'user' ou 'admin'."}), 400
 
-  return jsonify({"message": "Dados invalidos"}), 400
+    if username and password:
+        user = User(username=username, password=password, role=role)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuário cadastrado com sucesso como {role}."})
+
+    return jsonify({"message": "Dados inválidos"}), 400
 
 
-@app.route('/user/<int:id_user>', methods=["GET"])
+# Rota Listar User --------------
+@app.route("/user/<int:id_user>", methods=["GET"])
 @login_required
 def read_user(id_user):
-  user = User.query.get(id_user)
+    user = User.query.get(id_user)
 
-  if user:
-    return {"username": user.username}
-  
-  return jsonify({"message": "Usuario não encontrado"}), 404
+    if user:
+        return {"username": user.username}
 
-@app.route('/user/<int:id_user>', methods=["PUT"])
+    return jsonify({"message": "Usuario não encontrado"}), 404
+
+
+# Rota Atualizar User --------------
+@app.route("/user/<int:id_user>", methods=["PUT"])
 @login_required
 def update_user(id_user):
-  data = request.json
-  user = User.query.get(id_user)
+    data = request.json
+    user = User.query.get(id_user)
 
-  if id_user != current_user.id and current_user.role == "user":
-    return jsonify({"message": "Operação não permitida"}), 403
+    if current_user.role == "user" and current_user.id != id_user:
+        return jsonify({"message": "Operação não permitida"}), 403
 
-  if user and data.get("password"):
-    user.password = data.get("password")
-    db.session.commit()
+    if user and data.get("password"):
+        user.password = data.get("password")
+        db.session.commit()
+        return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
 
-    return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
-  
-  return jsonify({"message": "Usuario não encontrado"}), 404
+    return jsonify({"message": "Usuário não encontrado"}), 404
 
 
-if __name__ == '__main__':
-  app.run(debug=True)
-  
+# Rota Deletar User --------------
+@app.route("/user/<int:id_user>", methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if current_user.role != "admin":
+        return jsonify({"message": "Operação não permitida"}), 403
+
+    if id_user == current_user.id:
+        return jsonify({"message": "Deleção não permitida"}), 403
+
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
+
+    return jsonify({"message": "Usuario não encontrado"}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
