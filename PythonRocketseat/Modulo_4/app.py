7a595607503a9945e1f3d0c9532993e829b9d219
jones-bass <jonesbass.tb@gmail.com>
2025-07-09 21:33:23 -0300

    Add Get dos Users e Put dos Users

--- PythonRocketseat/Modulo_4/app.py
+++ PythonRocketseat/Modulo_4/app.py
@@ -55,6 +55,35 @@ def create_user():
 
   return jsonify({"message": "Dados invalidos"}), 400
 
+
+@app.route('/user/<int:id_user>', methods=["GET"])
+@login_required
+def read_user(id_user):
+  user = User.query.get(id_user)
+
+  if user:
+    return {"username": user.username}
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+@app.route('/user/<int:id_user>', methods=["PUT"])
+@login_required
+def update_user(id_user):
+  data = request.json
+  user = User.query.get(id_user)
+
+  if id_user != current_user.id and current_user.role == "user":
+    return jsonify({"message": "Operação não permitida"}), 403
+
+  if user and data.get("password"):
+    user.password = data.get("password")
+    db.session.commit()
+
+    return jsonify({"message": f"Usuário {id_user} atualizado com sucesso"})
+  
+  return jsonify({"message": "Usuario não encontrado"}), 404
+
+
 if __name__ == '__main__':
   app.run(debug=True)
   
