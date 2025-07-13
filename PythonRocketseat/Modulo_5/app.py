198143df8dedea669ef7c0b3f66089bb26cf2ce4
jones-bass <jonesbass.tb@gmail.com>
2025-07-13 14:57:59 -0300

    Add rota PIX confirmation

--- PythonRocketseat/Modulo_5/app.py
+++ PythonRocketseat/Modulo_5/app.py
@@ -3,6 +3,7 @@ from repository.database import db
 from datetime import datetime, timedelta
 from db_models.payment import Payment
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 
 @app.route("/payments/pix", methods=["POST"])
@@ -53,12 +55,38 @@ def get_image(file_name):
 
 @app.route("/payments/pix/confirmation", methods=["POST"])
 def pix_confirmation():
+
+    data = request.get_json()
+
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found"}), 404
+
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f"payment-confirmed-{payment.id}")
+
     return jsonify({"message": "The payment has been confirmed"})
 
 
 @app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if not payment:
+        return render_template("404.html")
+
+    if payment.paid:
+        return render_template(
+            "confirmed_payment.html", payment_id=payment.id, value=payment.value
+        )
 
     return render_template(
         "payment.html",
@@ -69,5 +97,10 @@ def payment_pix_page(payment_id):
     )
 
 
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected to the server")
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
